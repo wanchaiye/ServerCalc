e1fe79f871ab4a9aeca49d8ce8f35170485d8d07
wanchaiye <34498259+wanchaiye@users.noreply.github.com>
2017-12-13 22:09:12 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// var http = require('http');
+
+// //create a server object:
+// http.createServer(function (req, res) {
+// //   res.write('Hello World!'); //write a response to the client
+// //   res.end(); //end the response
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(req.url);
+//     res.end();
+// }).listen(8080); //the server object listens on port 8080
+'use strict'
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, resp){
+    resp.status(200).send('Hello World!!');
+});
+
+var server = app.listen(process.env.PORT || '8080', function(){
+    console.log('App listening on port %s', server.address().port);
+    console.log('Press Ctrl+C to quit');
+})
